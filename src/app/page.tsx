654a2922a27281f86b32dd541ae4215ff6f0378e
Alexander Löfstrand <alexander.lofstrand@tietoevry.com>
2025-10-02 14:49:18 +0200

    feat: Colors on border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,116 @@
 "use client";
 import { MusicCard, PlaceCardButton } from "@/components";
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Fragment, useState } from "react";
+
+export type MusicCardType = {
+  year: number;
+  title: string;
+  artist: string;
+};
+
+type GameState = "placing" | "won" | "lost" | "idle";
 
 export default function Page() {
-  const [musicCardYears, setMusicCardYears] = useState([
-    1967, 1987, 1999, 2000,
-  ]);
-  const [currentCard, setCurrentCard] = useState(2024);
+  const [success, setSuccess] = useState<boolean | null>(null);
+  const [gameState, setGameState] = useState<GameState>("placing");
+  const initialCards: MusicCardType[] = [
+    { year: 1967, title: "Purple Haze", artist: "Jimi Hendrix" },
+    { year: 1987, title: "With or Without You", artist: "U2" },
+    { year: 1999, title: "No Scrubs", artist: "TLC" },
+    { year: 2000, title: "Stan", artist: "Eminem" },
+  ];
+
+  const [musicCards, setMusicCards] = useState<MusicCardType[]>(initialCards);
+
+  const [currentCard, setCurrentCard] = useState<MusicCardType>({
+    year: 2024,
+    title: "Title",
+    artist: "Artist",
+  });
+
+  function generateNewCard() {
+    setCurrentCard({
+      year: Math.floor(Math.random() * (2025 - 1950 + 1)) + 1950,
+      title: "New Title",
+      artist: "New Artist",
+    });
+  }
 
   function placeCard(index: number, year: number) {
     if (index === 0) {
-      if (musicCardYears[0] >= year) {
-        setMusicCardYears([year, ...musicCardYears]);
-        setCurrentCard(Math.floor(Math.random() * (2025 - 1950 + 1)) + 1950);
+      if (musicCards[0].year >= year) {
+        setSuccess(true);
+        setMusicCards([
+          { year, title: "New Title", artist: "New Artist" },
+          ...musicCards,
+        ]);
       }
-    } else if (index === musicCardYears.length) {
-      if (musicCardYears[musicCardYears.length - 1] <= year) {
-        setMusicCardYears([...musicCardYears, year]);
-        setCurrentCard(Math.floor(Math.random() * (2025 - 1950 + 1)) + 1950);
+    } else if (index === musicCards.length) {
+      if (musicCards[musicCards.length - 1].year <= year) {
+        setSuccess(true);
+        setMusicCards([
+          ...musicCards,
+          { year, title: "New Title", artist: "New Artist" },
+        ]);
       }
     } else {
-      if (musicCardYears[index - 1] <= year && musicCardYears[index] >= year) {
-        const newYears = [...musicCardYears];
-        newYears.splice(index, 0, year);
-        setMusicCardYears(newYears);
-        setCurrentCard(Math.floor(Math.random() * (2025 - 1950 + 1)) + 1950);
+      if (
+        musicCards[index - 1].year <= year &&
+        musicCards[index].year >= year
+      ) {
+        const newCards = [...musicCards];
+        newCards.splice(index, 0, {
+          year,
+          title: "New Title",
+          artist: "New Artist",
+        });
+        setMusicCards(newCards);
+        setSuccess(true);
+      } else {
+        setSuccess(false);
       }
     }
+    setGameState("idle");
   }
 
   return (
     <main className="flex flex-col justify-center gap-8 p-8">
       <section className="flex justify-center">
-        <MusicCard year={currentCard} />
+        {gameState === "placing" ? (
+          <MusicCard musicCard={currentCard} hidden={true} />
+        ) : (
+          <div className="flex flex-col items-center gap-4">
+            <MusicCard
+              musicCard={currentCard}
+              hidden={false}
+              success={success}
+            />
+            <Button
+              onClick={() => {
+                setGameState("placing");
+                generateNewCard();
+              }}
+            >
+              Next Card
+            </Button>
+          </div>
+        )}
       </section>
       <section
         id="timeline"
         className="flex items-center gap-4 mt-12 justify-center overflow-x-scroll"
       >
-        <PlaceCardButton onClick={() => placeCard(0, currentCard)} />
-        {musicCardYears.map((year) => (
-          <>
-            <MusicCard key={year} year={year} />
+        <PlaceCardButton onClick={() => placeCard(0, currentCard.year)} />
+        {musicCards.map((card) => (
+          <Fragment key={card.year}>
+            <MusicCard musicCard={card} hidden={false} />
             <PlaceCardButton
               onClick={() =>
-                placeCard(musicCardYears.indexOf(year) + 1, currentCard)
+                placeCard(musicCards.indexOf(card) + 1, currentCard.year)
               }
             />
-          </>
+          </Fragment>
         ))}
       </section>
     </main>
--- src/components/musicCard.tsx
+++ src/components/musicCard.tsx
@@ -1,18 +1,42 @@
 import React from "react";
 import {
   Card,
+  CardDescription,
   // CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { MusicCardType } from "@/app/page";
 
-export function MusicCard({ year }: { year: number }) {
+export function MusicCard({
+  musicCard,
+  hidden,
+  success = null,
+}: {
+  hidden: boolean;
+  musicCard: MusicCardType;
+  success?: boolean | null;
+}) {
   return (
-    <Card className="w-25">
+    <Card
+      className="w-25"
+      style={{
+        border:
+          success === true
+            ? "2px solid green"
+            : success === false
+            ? "2px solid red"
+            : undefined,
+      }}
+    >
       <CardHeader className="text-center">
-        <CardTitle>{year}</CardTitle>
-        {/* <CardDescription>Håll till höger Svensson</CardDescription>
-          <CardDescription>The Telstars</CardDescription> */}
+        <CardTitle>{!hidden ? musicCard.year : "???"}</CardTitle>
+        {!hidden && (
+          <>
+            <CardDescription>{musicCard.title}</CardDescription>
+            <CardDescription>{musicCard.artist}</CardDescription>
+          </>
+        )}
       </CardHeader>
     </Card>
   );
